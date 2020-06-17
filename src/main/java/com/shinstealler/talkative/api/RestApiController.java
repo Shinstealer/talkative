d79f6d417c39d7162afc470aff8fb9206f38264f
shinstealer <coolldd890@gmail.com>
2020-06-17 16:58:54 +0900

    work: api controller

---
+++ src/main/java/com/shinstealler/talkative/api/RestApiController.java
@@ -0,0 +1,13 @@
+package com.shinstealler.talkative.api;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.extern.slf4j.Slf4j;
+
+@RestController
+@Slf4j
+@RequestMapping("/shinstealer/talkative")
+public class RestApiController {
+    
+}
